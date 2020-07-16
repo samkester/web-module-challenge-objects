03b557540ec71be35101759f4cc886a4a8579c5d
Sam Kester <samkester@gmail.com>
2020-07-16 13:07:25 -0600

    completed tasks 6-7

--- index.js
+++ index.js
@@ -93,9 +93,10 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
-    /* code here */
-  }
-  
+    return `${reviews[index].name} gave the restaurant a ${reviews[index].rating}-star review and their feedback was: ${reviews[index].feedback}`;
+}
+
+console.log(getReviewByIndex(reviews, 0));
 
 /* Task 7: Write a function to get information about the most recent review called `getLastReview`
 
@@ -106,10 +107,11 @@ and should return a string in the format `name} gave the restaurant a {rating}, 
 
 For example, if getLastReview is invoked passing the reviews array it will return `Reyna gave the restaurant a 3.5 star review and their feedback was: "this place is chill with really cool people, great for getting work done on weekdays"`.
 */
-function getLastReview(/* code here */) {
-    /* code here */
-  } 
+function getLastReview(reviews) {
+    getReviewByIndex(reviews, reviews.length - 1);
+} 
 
+console.log(getLastReview(reviews));
 
 ///////////////🍔☕️🍽 STRETCH🍔☕️🍽////////////////////
 
